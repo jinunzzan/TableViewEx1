a36c5be04d0f34eee8e46c4ba140de3d1c514fbc
jinunzzan <lovebox6607@gmail.com>
2022-10-25 03:16:29 +0900

    TableView

--- TableViewEx1/ViewController.swift
+++ TableViewEx1/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(indexPath.row)번째 cell"
+        content.image = UIImage(systemName: "bus")
+        content.secondaryText = "Secondary"
+        cell.contentConfiguration = content
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        label.text = "\(indexPath.row)번째 셀"
+    }
+    
+}
+extension ViewController: UITableViewDelegate {
+    
+}
